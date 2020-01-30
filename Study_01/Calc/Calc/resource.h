bc4fd59d8d13d37324668b9e4cc72ce8b498d67a
YangHyunSeung <41092020+zmfoq12345@users.noreply.github.com>
2020-01-30 21:57:36 +0900

    MFC_CALCULATOR

---
+++ Study_01/Calc/Calc/resource.h
@@ -0,0 +1,37 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Calc.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CALC_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON_ADD                  1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON_SUB                  1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON_MUL                  1004
+#define IDC_BUTTON4                     1005
+#define IDC_BUTTON_DIV                  1005
+#define IDC_BUTTON5                     1006
+#define IDC_BUTTON_RESET                1006
+#define IDC_BUTTON6                     1007
+#define IDC_BUTTON_QUIT                 1007
+#define IDC_BUTTON7                     1008
+#define IDC_BUTTON_EQU                  1008
+#define IDC_EDIT3                       1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
