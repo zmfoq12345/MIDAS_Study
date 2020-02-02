b562364178ba8b726ff66328451730868edf49e7
YangHyunSeung <41092020+zmfoq12345@users.noreply.github.com>
2020-02-02 18:21:05 +0900

    btn_classAdd

--- ClassAdd1/Resource.h
+++ ClassAdd1/Resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_CLASSADD1_DIALOG            102
 #define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
 
 // Next default values for new objects
 // 
@@ -14,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
